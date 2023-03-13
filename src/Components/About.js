927aad4786a5c118ebafee6e532ebdc909ac2376
MAsim336 <mohammad.family5g@gmail.com>
2023-03-13 23:53:01 +0530

    first commit

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,26 +7,6 @@ export default function About() {
         backgroundColor: 'white'
     })
 
-    const [btnText, setBtnText] = useState("Enable Dark Mode")
-
-    const toggleStyle = ()=> {
-        if(myStyle.color === 'black'){
-            setMyStyle({
-                color: 'white',
-                backgroundColor: 'black',
-                border: '1px solid white'
-            })
-            setBtnText("Enable Light Mode")
-        }
-        else{
-            setMyStyle({
-                color: 'black',
-                backgroundColor: 'white'
-            })
-            setBtnText("Enable Dark Mode");
-        }
-    }
-
   return (
     <div className='container' style={myStyle}>
         <h1 className='my-3'>About Us</h1>
